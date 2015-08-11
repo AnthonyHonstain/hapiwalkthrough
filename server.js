4b77bff6a339c5da6b094095ec7a0fa0f2b2b4fc
Anthony Honstain <honstain@gmail.com>
2015-08-11 12:39:48 -0700

    initial commit of hapi server

---
+++ server.js
@@ -0,0 +1,44 @@
+var Hapi = require('hapi');
+var Good = require('good');
+
+var server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+
+server.route({
+	method: 'GET',
+	path: '/',
+	handler: function (request, reply) {
+		reply('Hello, world!');
+	}
+});
+
+server.route({
+	method: 'GET',
+	path: '/{name}',
+	handler: function (request, reply) {
+		reply('Hello,' + encodeURIComponent(request.params.name) + '!');
+	}
+});
+
+server.register({
+	register: Good,
+	options: {
+		reporters: [{
+			reporter: require('good-console'),
+			events: {
+				response: '*',
+				log: '*'
+			}
+		}]
+	}
+}, function (err) {
+	if (err) {
+		throw err; // something bad happend loading the plugin
+	}
+
+	server.start(function () {
+		console.log('Server running at:', server.info.uri);
+	});
+});
+
